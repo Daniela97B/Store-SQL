8e927272327f9afecf10896879aef36ec98988a0
Daniela97B <daniigm97@gmail.com>
2022-05-22 07:14:26 -1200

    Initial commit

---
+++ script1.sql
@@ -0,0 +1,145 @@
+﻿--
+-- Base de datos: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `tipo_documento` varchar(255) NOT NULL,
+  `documento` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `id_producto`, `tipo_documento`, `documento`) VALUES
+(1, 8, 'CC', '1006120642'),
+(2, 2, 'CC', '1006120642'),
+(3, 3, 'CC', '1061806330');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `id_proveedor` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `id_proveedor`, `nombre`) VALUES
+(1, 1, 'Bombom'),
+(2, 1, 'Yogurt'),
+(3, 1, 'Gaseosa'),
+(4, 2, 'Galletas'),
+(5, 2, 'Pony Malta'),
+(6, 2, 'Chocoso'),
+(7, 3, 'Zucaritas'),
+(8, 3, 'Agua');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id_proveedor`, `nombre`) VALUES
+(1, 'Dani Dani'),
+(2, 'Sebas'),
+(3, 'Random');
+
+--
+-- BORRADOR LOGICO, FISICO Y MODIFICACIONES
+--
+
+DELETE FROM producto WHERE id_producto = 6;
+DELETE FROM producto WHERE id_producto = 4;
+UPDATE producto SET nombre = 'chocoso' WHERE id_producto = 1;
+UPDATE producto SET nombre = 'bombom' WHERE id_producto = 3;
+UPDATE producto SET nombre = 'chocorramo', id_proveedor =   '2' WHERE id_producto = 1;
+UPDATE producto SET nombre = 'pepsy', id_proveedor = '2' WHERE id_producto = 2;
+UPDATE producto SET nombre = 'DeTodito', id_proveedor = '1' WHERE id_producto = 5;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_proveedor` (`id_proveedor`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ script2.sql
@@ -0,0 +1,9 @@
+﻿--
+-- Base de datos: `store`
+--
+
+-- --------------------------------------------------------
+
+1. SELECT * FROM cliente c INNER JOIN producto p ON c.id_producto = p.id_producto WHERE c.tipo_documento = 'CC' AND c.documento = '1061806330';
+
+2. SELECT * FROM producto p INNER JOIN proveedor pro ON pro.id_proveedor = p.id_proveedor WHERE p.nombre = 'Zucaritas';
